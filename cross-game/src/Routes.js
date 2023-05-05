d87fd43c2ff0327203ceb3981ddf5dc903c73333
Gabriel Mello <Gabriel.mmello@sptech.school>
2023-05-05 00:30:22 -0300

    wip branch "telaCadastro"

--- cross-game/src/Routes.js
+++ cross-game/src/Routes.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/index.jsx";
+import Cadastro from './pages/Cadastro/Cadastro.jsx';
 
 function routes() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/cadastro" element={<Cadastro />} />
             </Routes>
         </BrowserRouter>
     );

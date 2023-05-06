dbc061a12b617511a1a2444b80882da6c07e361e
Mateus Pereira da Silva <99891990+mat-pereira@users.noreply.github.com>
2023-05-06 11:30:01 +0000

    add route teste

--- cross-game/src/Routes.js
+++ cross-game/src/Routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/index.jsx";
 import Cadastro from './pages/Cadastro/Cadastro.jsx';
+import Teste from './pages/Teste/index.jsx';
 
 function routes() {
     return (
@@ -9,6 +10,7 @@ function routes() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/cadastro" element={<Cadastro />} />
+                <Route path="/teste" element={<Teste />} />
             </Routes>
         </BrowserRouter>
     );

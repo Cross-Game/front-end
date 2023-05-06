b3cb251700481e419242ab008dee2302df8286ff
Mateus Pereira da Silva <99891990+mat-pereira@users.noreply.github.com>
2023-05-06 20:48:28 -0300

    Revert "Develop"

--- cross-game/src/index.js
+++ cross-game/src/index.js
@@ -1,14 +1,33 @@
 import "./assets/reset.css"
-import "./assets/global.css"
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
 import App from './App';
+import Teste from "./pages/Teste";
+
+
+// configuração do router
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+  {
+    path: "/teste",
+    element: <Teste />
+  }
+]);
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 

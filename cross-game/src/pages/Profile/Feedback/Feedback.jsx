38043585cfd3777547f9a2eef2e29c2b19d62f6c
Gabriel Mello <Gabriel.mmello@sptech.school>
2023-05-23 23:52:38 -0300

    wip: prototipo de card do feedback feito

--- cross-game/src/pages/Profile/Feedback/Feedback.jsx
+++ cross-game/src/pages/Profile/Feedback/Feedback.jsx
@@ -1,12 +1,48 @@
 import React from "react";
-import ProfileNavbar from "../../../components/ProfileNavbar/ProfileNavbar"
-import Sidebar from "../../../components/Sidebar/Sidebar"
+import ProfileNavbar from "../../../components/ProfileNavbar/ProfileNavbar";
+import Sidebar from "../../../components/Sidebar/Sidebar";
+import "./Feedback.css";
+
 
 function ProfileJogo() {
+
+    function adicionar() {
+        return (
+            <>
+                <div className="ProfileFeedbackContainer">
+                    <div className="ProfileFeedbackCard">
+                        <h4>Feedback</h4>
+                    </div>
+                    <div className="ProfileFeedbackCard">
+                        <h4>Feedback</h4>
+                    </div>
+                    <div className="ProfileFeedbackCard">
+                        <h4>Feedback</h4>
+                    </div>
+                    <div className="ProfileFeedbackCard">
+                        <h4>Feedback</h4>
+                    </div>
+                    <div className="ProfileFeedbackCard">
+                        <h4>Feedback</h4>
+                    </div>
+                    <div className="ProfileFeedbackCard">
+                        <h4>Feedback</h4>
+                    </div>
+                    <div className="ProfileFeedbackCard">
+                        <h4>Feedback</h4>
+                    </div>
+                    <div className="ProfileFeedbackCard">
+                        <h4>Feedback</h4>
+                    </div>
+                </div>
+            </>
+        )
+    }
     return (
         <>
             <ProfileNavbar feedbacks={{ color: '#fff', borderBottom: '2px solid #0f3' }}
                 sidebar={<Sidebar />}
+                adicionar={adicionar()}
             />
         </>
     )

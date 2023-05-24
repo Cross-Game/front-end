be74f866dc4bb34bb9436a58f8262b794d5500f3
mirandagabriel <gabriel.mello@alpe.com.br>
2023-05-24 18:31:16 -0300

    wip

--- cross-game/src/pages/Profile/Plataforma/Plataforma.jsx
+++ cross-game/src/pages/Profile/Plataforma/Plataforma.jsx
@@ -3,10 +3,20 @@ import ProfileNavbar from "../../../components/ProfileNavbar/ProfileNavbar"
 import Sidebar from "../../../components/Sidebar/Sidebar"
 
 function ProfileJogo() {
+    function adicionar() {
+        return (
+            <>
+            
+            </>
+        )
+    }
+
+    
     return (
         <>
             <ProfileNavbar plataformas={{ color: '#fff', borderBottom: '2px solid #0f3' }}
                 sidebar={<Sidebar />}
+                adicionar={adicionar()}
             />
         </>
     )

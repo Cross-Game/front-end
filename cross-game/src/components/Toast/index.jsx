1ad8cbb4a43c376a30cdffc8409f32aec7a713ba
Mateus Pereira da Silva <mateus.silva@genial.com.vc>
2023-05-27 09:39:19 -0300

    fix time toast

--- cross-game/src/components/Toast/index.jsx
+++ cross-game/src/components/Toast/index.jsx
@@ -10,7 +10,7 @@ const Toast = ({ type, message, onClose }) => {
     const timer = setTimeout(() => {
       setShow(false);
       onClose();
-    }, 90000);
+    }, 7000);
 
     return () => {
       clearTimeout(timer);

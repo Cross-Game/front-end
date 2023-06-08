502f71abec0cc2d6fa057e30b9de858c906d9a9e
LeonardoMS07 <leonardo.soares@sptech.school>
2023-06-08 17:34:40 -0300

    validação da plataforma

--- cross-game/src/pages/Profile/Plataforma/Plataforma.jsx
+++ cross-game/src/pages/Profile/Plataforma/Plataforma.jsx
@@ -108,25 +108,31 @@ function ProfileJogo() {
                         </span>
                     </div>
                 </div>
+
+
                 <div className="ProfilePlataformaContainer">
 
                     {plataformas.includes("PLAYSTATION") && (
-                    <CardPlay />
+                        <CardPlay />
                     )}
 
                     {plataformas.includes("XBOX") && (
-                    <CardXbox />
+                        <CardXbox />
                     )}
 
                     {plataformas.includes("DESKTOP") && (
-                    <CardPc />
+                        <CardPc />
                     )}
 
                     {plataformas.includes("MOBILE") && (
-                    <CardMobile />
+                        <CardMobile />
                     )}
 
+                    {!plataformas.includes("PLAYSTATION") && !plataformas.includes("XBOX") && !plataformas.includes("DESKTOP") && !plataformas.includes("MOBILE") && (
+                        <p className="ProfilePlataformaContainer">Nenhuma Plataforma foi encontrada para o usuário.</p>
+                    )}
                 </div>
+
                 {showModalAdicionarPlataforma && (
                     <Modal title="Plataformas" icon={<BsGridFill />} temFooter={true} ativarBotao={true} textButton="Adicionar" iconButton={<BsCheck />} clearAll={true} onClear={limparPlataformas} onClickButton={cadastrarPlataforma} onClose={() => setShowModalAdicionarPlataforma(false)}>
                         <div className="ModalPlataforma-cadastrarPlataforma">

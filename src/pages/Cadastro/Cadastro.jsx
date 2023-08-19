f85dcc0b78fb57299c163b733d66bf0a3a27b747
Gabriel Mello <gabriel.mmello@sptech.school>
2023-08-19 11:19:10 -0300

    Bug de validação dos cmapos resolvido

--- src/pages/Cadastro/Cadastro.jsx
+++ src/pages/Cadastro/Cadastro.jsx
@@ -121,14 +121,13 @@ function Cadastro() {
     };
 
     const validatePassword = (password) => {
-        // Lógica para validar a senha
         const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{12,}$/;
 
         if (!passwordRegex.test(password)) {
-            //setShowModalSenha(true);
+            setShowModalSenha(true);
             setvalidadoSenha(false);
-            setValidationMessageSenha('A senha deve conter pelo menos 12 caracteres sendo: 1 numero, 1 letra maiúscula, 1 letra minúscula e 1 caractere especial.');
-            mudarToast('erro', "A senha deve conter pelo menos 12 caracteres sendo: 1 número, 1 letra maiúscula, 1 letra minúscula e 1 caractere especial.");
+            setValidationMessageSenha('Deve conter 12 caracteres: 1 numero, 1 letra maiúscula, 1 letra minúscula e 1 caractere especial.');
+            mudarToast('erro', "Deve conter 12 caracteres: 1 número, 1 letra maiúscula, 1 letra minúscula e 1 caractere especial.");
         } else {
             setShowModalSenha(false);
             setValidationMessageSenha('');
@@ -137,11 +136,10 @@ function Cadastro() {
     };
 
     const validateEmail = (email) => {
-        // Lógica para validar o email
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
         if (!emailRegex.test(email)) {
-            //setShowModalEmail(true);
+            setShowModalEmail(true);
             mudarToast('erro', 'O email deve conter @ e "."')
             setValidadoEmail(false)
             setValidationMessageEmail('O email esta sem o @ ou "."');

b2df5d5eaccabdcc2fc9bb11a4d1dde6e812f450
Gabriel Mello <gabriel.mmello@sptech.school>
2023-10-01 13:27:51 -0300

    wip

--- src/pages/Profile/ProfileJogo/ProfileJogo.jsx
+++ src/pages/Profile/ProfileJogo/ProfileJogo.jsx
@@ -157,26 +157,30 @@ function ProfileJogo() {
         console.log("Meus Jogos")
     }
 
+    const handlerBuscarJogo = () => {
+        console.log("Buscando Jogos")
+    }
+
     function adicionar() {
         return (
             <>
                 <div className="ProfileJogoMiniContainer">
                     <div className="ProfileJogoContainerButtonAdicionar">
                         <span className="ProfileJogoButtonAdicionar" onClick={() => handlerMeusJogo()}>
-                            Jogos
+                            Meus Jogos
                             <GiConsoleController className="ProfileIconAdicionar" />
                         </span>
-                        <span className="ProfileJogoButtonAdicionar" onClick={() => console.log("chamar a api de lista de jogo")}>
+                        <span className="ProfileJogoButtonAdicionar" onClick={() => console.log("Adicionando jogo")}>
                             Adicionar
                             <AiFillPlusCircle className="ProfileIconAdicionar" />
                         </span>
                     </div>
                 </div>
                 <div className="ProfileJogoCardContainerTitulo">
-                    <h2 className="tituloInputGame">Jogos selecionado por você</h2>
+                    
                     <div className="search">
                         <input type="text" className="search__input" placeholder="Busque pelo nome do jogo"/>
-                            <button className="search__button">
+                            <button className="search__button" onClick={handlerBuscarJogo}>
                                 <svg className="search__icon" aria-hidden="true" viewBox="0 0 24 24">
                                     <g>
                                         <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path>
